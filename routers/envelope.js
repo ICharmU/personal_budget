8927c673b53b329775a8e0e2e4b0289d589ad794
Sean <seanisloveseanislife@gmail.com>
2023-07-29 18:30:51 -0700

    corrected for loops for budget checks and changed food to type

--- routers/envelope.js
+++ routers/envelope.js
@@ -9,37 +9,48 @@ let budget = {
     food: undefined,
     housing: undefined,
     gas: undefined,
-    utilities: undefined
+    utilities: undefined,
+    totalBudget: undefined
 }
 
 // http requests
 envelopeRouter.param('type', (req, res, next, type) => {
     let newBudget = req.query.budgetAmount;
     let budgetType = false;
-    for (let i = 0; i<budget.length; i++) {
-        if (type == budget[i]) {
-            i = budget.length;
+    let budgetKeys = Object.keys(budget);
+    for (let i = 0; i<budgetKeys.length; i++) {
+        if (type == budgetKeys[i]) {
+            i = budgetKeys.length;
             budgetType = true;
         }
     }
     if (newBudget && budgetType) {
-        budget.type = newBudget;
+        budget[type] = newBudget;
         next();
     } else {
         res.status(404).send();
     }
 });
 
-envelopeRouter.get('/', (req, res, next) => {
+// retrieve total budget
+envelopeRouter.get('/:type', (req, res, next) => {
     let tot = 0;
-    for (let i = 0; i<budget.length; i++) {
-        if (budget[i] != undefined) {
-            tot += budget[i];
+    let budgetKeys = Object.keys(budget);
+    for (let i = 0; i<budgetKeys.length; i++) {
+        if (budgetKeys[i] != undefined) {
+            tot += budgetKeys[i];
         }
     }
+    budget[totalBudget] = tot;
     res.status(200).send(tot);
 });
 
+// retrieve all budgets
+envelopeRouter.get('/', (req, res, next) => {
+    res.status(200).send(budget);
+});
+
+// retrieve budget of type (key from budget object)
 envelopeRouter.get('/:type', (req, res, next) => {
     res.status(200).send(newBudget);
 });
@@ -59,7 +70,9 @@ envelopeRouter.post('/:type', (req, res, next) => {
 
 envelopeRouter.delete('/:type', (req, res, next) => {
     if (newBudget) {
-        delete budget['food'];
+        delete budget[type];
         res.status(200).send();
     }
 });
+
+// update object access to be brackets instead of dot notation
