2e8a27451b09b5a0b2615f415c453de570f7bfc0
Sean <seanisloveseanislife@gmail.com>
2023-07-29 18:58:14 -0700

    added transfer function and post request

--- routers/envelope.js
+++ routers/envelope.js
@@ -34,15 +34,7 @@ envelopeRouter.param('type', (req, res, next, type) => {
 
 // retrieve total budget
 envelopeRouter.get('/:type', (req, res, next) => {
-    let tot = 0;
-    let budgetKeys = Object.keys(budget);
-    for (let i = 0; i<budgetKeys.length; i++) {
-        if (budgetKeys[i] != undefined) {
-            tot += budgetKeys[i];
-        }
-    }
-    budget[totalBudget] = tot;
-    res.status(200).send(tot);
+    res.status(200).send(totalBudget);
 });
 
 // retrieve all budgets
@@ -50,29 +42,55 @@ envelopeRouter.get('/', (req, res, next) => {
     res.status(200).send(budget);
 });
 
-// retrieve budget of type (key from budget object)
+// retrieve budget of type
 envelopeRouter.get('/:type', (req, res, next) => {
     res.status(200).send(newBudget);
 });
 
-
+// update budget of type
 envelopeRouter.put('/:type', (req, res, next) => {
-    budget.food = newBudget;
+    budget[type] = newBudget;
     res.status(200).send(newBudget);
 });
 
-envelopeRouter.post('/:type', (req, res, next) => {
-    if (newBudget) {
-        envelopeRouter[type] = newBudget;
+// add more budget envelopes to budget
+envelopeRouter.post('/:newType', (req, res, next) => {
+    let newType = req.query.newType;
+    let newBudget = req.query.budgetAmount;
+    if (newType && newBudget) {
+        budget[newType] = newBudget;
+        res.status(200).send(newBudget);
+    } else {
+        res.status(404).send();
     }
-    res.status(200).send(newBudget);
+    
 });
 
+// deletes type from budget
 envelopeRouter.delete('/:type', (req, res, next) => {
     if (newBudget) {
         delete budget[type];
         res.status(200).send();
     }
+});
+
+// function to add transfer amount of money to budgetType1 from budgetType2
+const transferBudget = (req, res, next) => {
+    req.body.budget1 = budget1;
+    req.body.budget2 = budget2;
+    req.body.transfer = transfer;
+    if (budget[budget1] && budget[budget2] && transfer) {
+        budget[budget1] += transfer;
+        budget[budget2] -= transfer;
+        next();
+    } else {
+        res.status(404).send();
+    }
+};
+
+
+envelopeRouter.post('/', transferBudget, (req, res, next) => {
+    res.status(200).send(transfer);
 });
 
 // update object access to be brackets instead of dot notation
